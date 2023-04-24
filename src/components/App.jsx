6ad6725a32137edf19e3a361ac6f2c3880e320e1
charlegit <93894121+charlegit@users.noreply.github.com>
2023-04-25 06:53:39 +0700

    update this morning

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Header from "./Header";
 
 function App() {
   return (
     <div>
-      <h1>Hello App!</h1>
+      <Header/>
+      {/* <h1>Hello</h1> */}
     </div>
   );
 }
